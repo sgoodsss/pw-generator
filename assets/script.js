211c1aee9f78e67183fa17d624c34db84d617982
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-16 18:53:18 -0400

    added pseudocode for generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log(`Hey!  You clicked the button.`);
+  // 1. Prompt the user with the password criteria
+  //    a. Password length between 8 and 128
+  //    b. Lowercase, Uppercase, Numbers, Special Characters
+  
+  // 2. Validate the input
+
+  // 3. Generate password
+
+  // 4.  Display the generated password on the page
+  return `Generated password will go here!`;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
