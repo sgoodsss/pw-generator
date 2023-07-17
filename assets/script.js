4493256bad5669ba3044c7fb82a441028008f61a
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-17 18:26:04 -0400

    Created prompt for password criteria

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,31 @@ function generatePassword() {
   // 1. Prompt the user with the password criteria
   //    a. Password length between 8 and 128
   //    b. Lowercase, Uppercase, Numbers, Special Characters
-  
+      var criteriaPrompt = prompt(`How long would you like your password to be?`, `Pick a number 8-128`);
+      
+      criteriaPrompt = parseInt(criteriaPrompt);
+      if (isNaN (criteriaPrompt)) {
+        alert(`Please choose a number and try again! :)`)
+      }
+      else {
+        if (criteriaPrompt < 8) {
+          alert(`Password cannot be less than 8 characters.`)
+        }
+        else if (criteriaPrompt > 128) {
+          alert(`Password cannot be larger than 128 characters.`)
+        }
+        // var lowercasePrompt = prompt () ask if they want lowercase characters
+      }
+    //  Use a switch instead of if else statement
+      if (criteriaPrompt < 8) {
+        alert(`Sorry, please choose a longer password.`);
+      } else if ({
+
+      } else {
+
+      }
+
+      // how do I say number values only
   // 2. Validate the input
 
   // 3. Generate password
