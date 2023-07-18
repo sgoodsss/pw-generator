09f9a2e30474e29cb2eaeb83ae5636eb4b7792d3
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-18 12:04:47 -0400

    Added values to specialCharacters array

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 var lowercase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
 var uppercase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
-var specialCharacters = [`!`,``, ];
+var specialCharacters = [`!`,`~`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`, `-`, `_`, `+`, `{`, `}`, `[`, `]`, ':', `;`, `'`, '"', `<`, `>`, `?`];
 var numbers = [`0`, `1`, `2`]
 var storeArray = [];
 var character = [];
