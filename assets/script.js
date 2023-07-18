e96fa1aa33659139f719cc5dedb14d7d4250bb58
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-18 12:00:42 -0400

    Worked with tutor Savien Love

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 var lowercase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
-var uppercase
+var uppercase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
+var specialCharacters = [`!`,``, ];
+var numbers = [`0`, `1`, `2`]
+var storeArray = [];
+var character = [];
 
 function generatePassword() {
   console.log(`Hey!  You clicked the button.`);
@@ -14,23 +18,40 @@ function generatePassword() {
       criteriaPrompt = parseInt(criteriaPrompt);
       if (isNaN (criteriaPrompt)) {
         alert(`Please choose a number and try again! :)`)
-      }
-      else {
-        if (criteriaPrompt < 8) {
-          alert(`Password cannot be less than 8 characters.`)
-        }
-        else if (criteriaPrompt > 128) {
-          alert(`Password cannot be larger than 128 characters.`)
-        }
-        // var lowercasePrompt = prompt () ask if they want lowercase characters
+        return;
       }
 
-  // 2. Validate the input
+      if (criteriaPrompt < 8 || criteriaPrompt > 128) {
+        alert(`Password cannot be less than 8 characters or larger than 128 characters.`)
+        return;
+        }
+    
+      var lowercasePrompt = confirm (`Do you want to use lowercase letters?`)
+      console.log(lowercasePrompt)
+      if (lowercasePrompt == true) {
+        storeArray = lowercase.concat(storeArray);
+        // storeArray = uppercase.concat(storeArray);
+      }
+
+      var uppercasePrompt = confirm (`Do you want to use uppercase letters?`)
+      console.log(uppercasePrompt)
+      if (uppercasePrompt == true) {
+        storeArray = uppercase.concat(storeArray);
+      }
+      console.log(storeArray);
 
   // 3. Generate password
-
+  for (var i = 0; i < criteriaPrompt; i++) {
+    var storeNumber = Math.floor(Math.random() * storeArray.length);
+    console.log(storeNumber);
+    var index = storeArray[storeNumber];
+    console.log (index);
+    character.push(index);
+    // character.push(storeArray[storeNumber]);
+    console.log(character);
+  }
   // 4.  Display the generated password on the page
-  return `Generated password will go here!`;
+  return character.join(``);
 }
 
 // Write password to the #password input
