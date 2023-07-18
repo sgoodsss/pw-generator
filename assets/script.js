ed2a4e3a808485a955915141589d8a1b20386b3d
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-18 12:05:55 -0400

    Added values to numbers array

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
 var uppercase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
 var specialCharacters = [`!`,`~`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`, `-`, `_`, `+`, `{`, `}`, `[`, `]`, ':', `;`, `'`, '"', `<`, `>`, `?`];
-var numbers = [`0`, `1`, `2`]
+var numbers = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]
 var storeArray = [];
 var character = [];
 
